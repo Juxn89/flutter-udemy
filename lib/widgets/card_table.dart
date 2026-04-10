c643a6283a48154875b2c32578d5f34857a75898
Juan Gómez <gb.jc@outlook.com>
2026-04-10 23:46:13 +0000

    Compise designe: Refactoring.

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -53,33 +53,49 @@ class _SingleCard extends StatelessWidget {
 
 	@override
 	Widget build(BuildContext context) {
-		return Container(
-			margin: EdgeInsets.all(15),
-			child: ClipRRect(
-				borderRadius: BorderRadius.circular(20),
-				child: BackdropFilter(
-					filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-					child: Container(
-						height: 180,
-						decoration: BoxDecoration(
-							color: Color.fromRGBO(62, 66, 107, 0.7),
-							borderRadius: BorderRadius.circular(20)
-						),
-						child: Column(
-							mainAxisAlignment: MainAxisAlignment.center,
-							children: [
-								CircleAvatar(
-									backgroundColor: color,
-									radius: 30,
-									child: Icon(icon, size: 35, color: Colors.white),
-								),
-								SizedBox(height: 10,),
-								Text(text, style: TextStyle(color: color, fontSize: 18),)
-							],
-						),
+		return _CardBackground(
+			child: Column(
+				mainAxisAlignment: MainAxisAlignment.center,
+				children: [
+					CircleAvatar(
+						backgroundColor: color,
+						radius: 30,
+						child: Icon(icon, size: 35, color: Colors.white),
 					),
-				),
-			),
+					SizedBox(height: 10,),
+					Text(text, style: TextStyle(color: color, fontSize: 18),)
+				],
+			)
 		);
 	}
+}
+
+class _CardBackground extends StatelessWidget {
+  const _CardBackground({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    	margin: EdgeInsets.all(15),
+    	child: ClipRRect(
+    		borderRadius: BorderRadius.circular(20),
+    		child: BackdropFilter(
+    			filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+    			child: Container(
+    				height: 180,
+    				decoration: BoxDecoration(
+    					color: Color.fromRGBO(62, 66, 107, 0.7),
+    					borderRadius: BorderRadius.circular(20)
+    				),
+    				child: child,
+    			),
+    		),
+    	),
+    );
+  }
 }
